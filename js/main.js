ee3a7484a591f300a331c4f9c261fa1963e44c98
Earlyrise <69732417+Earlyrise@users.noreply.github.com>
2020-09-09 00:48:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".photo__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../Kuba/img/arrow__right.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../Kuba/img/arrow__left.svg" alt=""></button>',
+  });
+});

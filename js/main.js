5b3907680ceca109d57d6c18c3fa2310884db0a4
Earlyrise <69732417+Earlyrise@users.noreply.github.com>
2020-09-09 15:49:09 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
   $(".photo__slider").slick({
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="./img/arrow__right.svg" alt=""></button>',
+      '<button type="button" class="slick-prev"><img src="../img/arrow__right.svg" alt=""></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="./img/arrow__left.svg" alt=""></button>',
+      '<button type="button" class="slick-next"><img src="../img/arrow__left.svg" alt=""></button>',
   });
 });
